72c471c5c81c315f601f0144973913a1f6c3b6e0
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-02-26 11:12:56 -0600

    create component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-// add css inline to components
-// add two css inline to components
-import logo from './logo.svg';
-import './App.css';
-
-const shadow = {
-  boxShadow: '0 0 20px rgba(0,0,0,.5)'
-}
-
-const estilo = (bg = '#000', color = "#fff") => ({
-  backgroundColor: bg,
-  color: color,
-  padding: '1rem',
-  margin: '3rem 1rem',
-})
+import Button from './Button'
 
 const App = () => {
   return (
     <div>
-      <div style={{ ...shadow, ...estilo('#057','#f56')}}>
-        alvaro
-      </div>
-      <div style={{ ...shadow, ...estilo('#fa4','#fa51f3')}}>
-        alvaro
-      </div>
-      <div style={{ ...shadow, ...estilo()}}>
-        alvaro
-      </div>
+      <h1 onClick={(e) => console.log("click h1")}>hi</h1>
+      <Button onClick={ () => console.log("click btn")}>send</Button>
     </div>
   );
 }
---
+++ src/intro-app.js
@@ -0,0 +1,18 @@
+// add import css
+
+// import logo from './logo.svg';
+// import './App.css';
+
+import './main.css';
+
+const App = () => {
+  return (
+    <div>
+      <div class="my-class">
+        alvaro
+      </div>
+    </div>
+  );
+}
+
+export default App;
