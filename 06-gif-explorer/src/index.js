36e3e3d23dbb21e981f0fd1eff719309a8dd2f45
chronosceptor <chronosceptor@chronosceptors-MacBook-Pro.local>
2022-06-27 16:45:17 -0500

    refresh projects

--- 06-gif-explorer/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import GifExplorerApp from './GifExplorerApp';
-
-const divRoot = document.querySelector('#root');
-const root = createRoot( divRoot );
-root.render( <GifExplorerApp /> );
